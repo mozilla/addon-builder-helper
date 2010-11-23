fdea1213f735a6dc3b7535018b25476ba70f099f
Atul Varma <varmaa@gmail.com>
2010-11-23 15:00:06 -0500

    Better debug output for build.py.

--- build.py
+++ build.py
@@ -9,12 +9,14 @@ except ImportError:
 
 def make_simple_xpi(msg, outfile):
   dest = os.path.join("examples", "sample-web-page", outfile)
-  print "creating %s..." % dest
-  subprocess.check_call([
+  args = [
     "cfx", "xpi",
     "-p", os.path.join("examples", "simple"),
     "--static-args=%s" % json.dumps({"message": msg})
-  ], stdout=subprocess.PIPE)
+  ]
+  print "executing: %s" % " ".join(args)
+  subprocess.check_call(args, stdout=subprocess.PIPE)
+  print "renaming generated addon to %s." % outfile
   shutil.move("simple.xpi", dest)
 
 make_simple_xpi("addon ONE", "simple1.xpi")
