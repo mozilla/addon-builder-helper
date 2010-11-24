e14961109d5439ff107964449fd2e8b9cb10274e
Atul Varma <varmaa@gmail.com>
2010-11-24 10:09:33 -0500

    Changed cmdline args passed to cfx to accommodate recent changes to its arguments on trunk.

--- build.py
+++ build.py
@@ -11,8 +11,8 @@ def make_simple_xpi(msg, outfile):
   dest = os.path.join("examples", "sample-web-page", outfile)
   args = [
     "cfx", "xpi",
-    "-p", os.path.join("examples", "simple"),
-    "--static-args=%s" % json.dumps({"message": msg})
+    "--pkgdir", os.path.join("examples", "simple"),
+    "--static-args", json.dumps({"message": msg})
   ]
   print "executing: %s" % " ".join(args)
   subprocess.check_call(args, stdout=subprocess.PIPE)
